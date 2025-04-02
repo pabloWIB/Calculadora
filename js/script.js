268795d6d453a0bf91fac73dbfcf6ae89e1670a7
Pablo Nieto <wibinweb@gmail.com>
2025-04-01 22:32:17 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+import './prevents/script.js';
+import './cursor/script.js';
+import './scrolling/script.js';
+
+
+import './nav/script.js';
+import './header/script.js';
+import './main/script.js'
+import './aside/script.js';
+// import './section/script.js';
+// import './about/script.js';
+import './skills/script.js';
+import './footer/script.js';
+
+Splitting();
